ecc6b429d1c35d6591039506d208f9573c90738e
Geordinateur <emuricardo@gmail.com>
2021-08-03 16:32:00 +0200

    backend: gif deleting is ok

--- backend/controllers/gif.js
+++ backend/controllers/gif.js
@@ -3,7 +3,6 @@ const Gif = db.gifs;
 const Op = db.Sequelize.Op;
 
 exports.create = (req, res, next) => {
-	console.log('Object: ' + req.body);
 	const Gifing = Gif.build({
 		title: req.body.title,
 		url: req.body.url,
@@ -17,12 +16,19 @@ exports.create = (req, res, next) => {
 };
 
 exports.delete = (req, res, next) => {
-	console.log('delete');
-	Gif.findOne({where: { _id: req.params.id }})
+	console.log(req.params.id);
+	//	Gif.findOne({where: { _id: req.params.id }})
+	Gif.findByPk(req.params.id)
 		.then(gif => {
-			console.log(gif);
+			if(gif !== null) {
+				Gif.destroy({where: { id: req.params.id}})
+					.then(() => res.status(201).json({ 'message': 'Objet supprimé!' }))
+					.catch(error => res.status(500).json({ error }));
+			}else{
+				res.status(404).json({'error': 'Objet non trouvé.'});
+			}
 		})
-		.catch(error => res.status(500).json({ error }));
+		.catch(error => res.status(401).json({ error }));
 };
 
 exports.getAll = (req, res, next) => {
